4b82dabe10589ffff268e19c433a25f206173c0d
TomBPro <tbalieupro@gmail.com>
2023-06-27 13:19:01 +0200

    Made basic template

---
+++ src/components/CocktailCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+        {{ index }} - {{ cocktail.name }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CocktailCard',
+    props: {
+        cocktail: Object
+    }
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/CocktailsVisualizer.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="center">
+        <h1>Bonjour !</h1>
+        <div v-if="cocktails">
+            <p>Voici quelques cocktails que j'aimerais vous proposer.</p>
+            <div v-for="cocktail in cocktails" :key="cocktail">
+                <CocktailCard props=cocktail />
+            </div>
+        </div>
+        <div v-else>
+            <p>Je n'ai pas de cocktails à vous proposer pour le moment. Repassez plus tard !</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import CocktailCard from './CocktailCard.vue';
+
+export default {
+    name: 'CocktailsVisualizer',
+    components: {
+        CocktailCard
+    },
+    props: {
+        cocktails: Array
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+
+.center {
+    text-align: center;
+}
+
+p {
+    font-size: 1.5em;
+}
+</style>
